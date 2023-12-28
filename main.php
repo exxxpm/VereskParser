0558adbd77f9a7cf65faf269f432678059c0581a
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-12-28 19:41:23 +0400

    Initial Commit

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+setlocale(LC_ALL, 'ru_RU');
+date_default_timezone_set('Europe/Moscow');
+header('Content-type: text/html; charset=utf-8');
+
+require __DIR__ . 'vendor/autoload.php';
